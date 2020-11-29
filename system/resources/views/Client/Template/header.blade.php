a9336f0df9c5b0af51ca3145c3df19a1c69a1e2a
oktakharis <okta.kharis@gmail.com>
2020-11-30 04:21:09 +0700

    Initial commit

---
+++ system/resources/views/Client/Template/header.blade.php
@@ -0,0 +1,13 @@
+<header class="masthead" style="background-image: url('{{url('public')}}/client/img/DOTA2.png')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="site-heading">
+            <h1>BUDAK - GABEN</h1>
+            <span class="subheading"> Sebuah ARTIKEL tentang DOTA 2 Untuk memperbanyak BUDAK GABEN </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
---
+++ system/resources/views/Client/about.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title> BUDAK - GABEN </title>
+
+  <!-- Bootstrap core CSS -->
+  @include('Client.Template.css')
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  @include('Client.Template.navigation')
+
+  <!-- Page Header -->
+  @include('Client.Template.headerabout')
+
+  <!-- Main Content -->
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 col-md-10 mx-auto">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe nostrum ullam eveniet pariatur voluptates odit, fuga atque ea nobis sit soluta odio, adipisci quas excepturi maxime quae totam ducimus consectetur?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius praesentium recusandae illo eaque architecto error, repellendus iusto reprehenderit, doloribus, minus sunt. Numquam at quae voluptatum in officia voluptas voluptatibus, minus!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut consequuntur magnam, excepturi aliquid ex itaque esse est vero natus quae optio aperiam soluta voluptatibus corporis atque iste neque sit tempora!</p>
+      </div>
+    </div>
+  </div>
+
+  <hr>
+
+  <!-- Footer -->
+  @include('Client.Template.footer')
+
+  @include('Client.Template.js')
+
+</body>
+
+</html>
